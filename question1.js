f8672ae3214627a4d8cf3284f46feafb14f9a31d
unknown <YY315@ccs.local>
2025-03-11 20:13:49 +0800

    Update question1.js

--- question1.js
+++ question1.js
@@ -1,7 +1,9 @@
-function countProperties(obj) {
+function countProperties(obj) 
+{
+// Get an array of the objects property 
     const keys = Object.keys(obj);
+// Return the length of the array
     return keys.length;
   }
-
 console.log(countProperties({ a: 1, b: 2, c: 3 })); 
   
